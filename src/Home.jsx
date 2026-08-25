3765e48890802f97d5d7b58f8622c3432ea2d7ed
Joey Bezner <jmbez@comcast.net>
2026-08-25 01:01:12 -0400

    featured fixes

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,16 @@
 import { useState, useEffect } from "react";
 import { supabase } from "./lib/supabaseClient";
 import './Home.css'
-import { IoLocationOutline } from "react-icons/io5";
+import { IoChevronBack, IoLocationOutline } from "react-icons/io5";
 import { useSearchParams } from "react-router-dom";
+import { GoDotFill } from "react-icons/go";
 
 export default function Home(){
     const [loading, setLoading] = useState(false);
     const [listings, setListings] = useState([]);
     const [searchParams, setSearchParams] = useSearchParams();
+
+    const [image, setImage] = useState(0);
 
     async function getListings(){
         setLoading(true);
@@ -40,15 +43,31 @@ export default function Home(){
                 <p>Multimedia Artist</p>
                 <p><IoLocationOutline /> Brooklyn, NY</p>
             </div>
-            <div style={{textAlign:"center"}}>
+            <div className="featured-container" style={{textAlign:"center"}}>
                 <h3 style={{marginBottom:"1rem"}}>Featured Works</h3>
                 <div className="featured">
+                    <button className="d-button left" onClick={() => setImage(image - 1 < 0 ? listings.length - 1 : image - 1)}>{<IoChevronBack />}</button>
+                    <button className="d-button right" onClick={() => setImage((image + 1) % listings.length)}>{<IoChevronBack />}</button>
+                    <div className="d-dots">
+                        {
+                            listings.map((element, index, key) => (
+                                <GoDotFill className={`d-dot ${index == image ? "selected" : ""}`} key={element.id} />
+                            ))
+                        }
+                    </div>
+                    {listings.length > 0 && 
+                        <div className="listing" key={listings[image].id} onClick={() => setSearchParams({"listing": listings[image].id})}>
+                            <img src={listings[image].img[0]} alt="Artwork Image" className="listing-image" />
+                        </div>
+                    }
+                </div>
+                {/* <div className="featured">
                     {listings.map((listing, key) => (
                         <div className="listing" key={listing.id} onClick={() => setSearchParams({"listing": listing.id})}>
                             <img src={listing.img[0]} alt="Artwork Image" className="listing-image" />
                         </div>
                     ))}
-                </div>
+                </div> */}
             </div>
         </div>
     );
